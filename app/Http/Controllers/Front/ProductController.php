072d89571d9821f7c4d36db9bbb97175e7b69463
Hibe Alsalih <hiba@example.com>
2026-06-03 19:43:06 +0300

    Refactor pages to Laravel MVC controllers

---
+++ app/Http/Controllers/Front/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return view('products');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Front\HomeController;
+use App\Http\Controllers\Front\ProductController;
+use App\Http\Controllers\Front\CartController;
+use App\Http\Controllers\Front\ContactController;
+use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/products', function () {
-    return view('products');
-});
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 
-Route::get('/cart', function () {
-    return view('cart');
-});
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 
-Route::get('/admin', function () {
-    return view('admin');
-});
+Route::get('/admin', [DashboardController::class, 'index'])->name('admin.dashboard');
